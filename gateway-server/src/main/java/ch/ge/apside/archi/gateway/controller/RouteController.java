1f649d22379976c6f4c6b347fe9ad3cd5e41b767
El Hadi ELOUARET <el-hadi.elouaret@apside.com>
2024-09-03 01:36:40 +0200

    Gateway config

--- gateway-server/src/main/java/ch/ge/apside/archi/gateway/controller/RouteController.java
+++ gateway-server/src/main/java/ch/ge/apside/archi/gateway/controller/RouteController.java
@@ -1,17 +1,58 @@
 package ch.ge.apside.archi.gateway.controller;
 
 //import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 @RestController
-@RequestMapping("/circuitbreakerfallback")
+//@RequestMapping("/circuitbreakerfallback")
 public class RouteController {
 
-    @RequestMapping("/")
+    private static final String SERVICE_NAME = "cart-service";
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @RequestMapping("/get-greeting-no-feign")
+    public String greeting(Model model) {
+
+        InstanceInfo service = eurekaClient
+                .getApplication(SERVICE_NAME)
+                .getInstances()
+                .get(0);
+
+        String hostName = service.getHostName();
+        int port = service.getPort();
+
+        URI url = URI.create("http://" + hostName + ":" + port + "/greeting");
+
+        ResponseEntity<String> response = new RestTemplate().getForEntity(url, String.class);
+
+        model.addAttribute("greeting", response.getBody());
+
+        return "page-view";
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return String.format("Index from '%s'!", eurekaClient.getApplication(appName).getName());
+    }
+
+
+    @RequestMapping("/circuitbreakerfallback")
     public String circuitbreakerfallback() {
         return "This is a fallback";
     }
---
+++ gateway-server/src/main/java/ch/ge/apside/archi/gateway/router/GatewayRouter.java
@@ -0,0 +1,106 @@
+package ch.ge.apside.archi.gateway.router;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayRouter {
+
+    @Value("${cart.service.url}")
+    private String cartServiceUrl;
+
+    @Value("${item.service.url}")
+    private String itemServiceUrl;
+
+//    private static final String CART_SERVICE_NAME = "cart-service";
+//    private static final String ITEM_SERVICE_NAME = "item-service";
+
+    @Autowired
+    private EurekaClient eurekaClient;
+
+//    public String getCartServiceUrl() {
+//        InstanceInfo cartService = eurekaClient
+//                .getApplication(CART_SERVICE_NAME)
+//                .getInstances()
+//                .get(0);
+//        String cartServiceHostName = cartService.getHostName();
+//        int port = cartService.getPort();
+//        return "http://" + cartServiceHostName + ":" + port + "/";
+//    }
+//
+//    public String getItemServiceUrl() {
+//        InstanceInfo itemService = eurekaClient
+//                .getApplication(ITEM_SERVICE_NAME)
+//                .getInstances()
+//                .get(0);
+//        String itemServiceHostName = itemService.getHostName();
+//        int port = itemService.getPort();
+//        return "http://" + itemServiceHostName + ":" + port + "/";
+//    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route(p -> p
+                        .path("/item")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri(itemServiceUrl))
+                .route(p -> p
+                        .path("/cart")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri(cartServiceUrl))
+//                .route(p -> p
+//                        .host("*.circuitbreaker.com")
+//                        .filters(f -> f
+//                                .circuitBreaker(config -> config
+//                                        .setName("mycmd")
+//                                        .setFallbackUri("forward:/fallback")))
+//                        .uri(httpUri))
+                .build();
+    }
+    /*
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        //@formatter:off
+        return builder.routes()
+                .route(p -> p
+                        .path("/get-item")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://localhost:8081/item/"))
+                .route(p -> p
+                        .path("/get-cart")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://localhost:8082/cart/"))
+//                .route("host_route", r -> r.host("*.myhost.org")
+//                        .uri("http://httpbin.org"))
+//                .route("rewrite_route", r -> r.host("*.rewrite.org")
+//                        .filters(f -> f.rewritePath("/foo/(?<segment>.*)",
+//                                "/${segment}"))
+//                        .uri("http://httpbin.org"))
+//                .route("circuitbreaker_route", r -> r.host("*.circuitbreaker.org")
+//                        .filters(f -> f.circuitBreaker(c -> c.setName("slowcmd")))
+//                        .uri("http://httpbin.org"))
+//                .route("circuitbreaker_fallback_route", r -> r.host("*.circuitbreakerfallback.org")
+//                        .filters(f -> f.circuitBreaker(c -> c.setName("slowcmd").setFallbackUri("forward:/circuitbreakerfallback")))
+//                        .uri("http://httpbin.org"))
+//                .route("limit_route", r -> r
+//                        .host("*.limited.org").and().path("/anything/**")
+//                        .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
+//                        .uri("http://httpbin.org"))
+//                .route("websocket_route", r -> r.path("/echo")
+//                        .uri("ws://localhost:9000"))
+                .build();
+        //@formatter:on
+    }
+*/
+//    @Bean
+//    RedisRateLimiter redisRateLimiter() {
+//        return new RedisRateLimiter(1, 2);
+//    }
+}
